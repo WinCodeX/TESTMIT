e1cec3341c0a67680495baeddba7f14107dcf3d6
WinCodeX <glenwinterg970@gmail.com>
2023-02-22 14:57:05 +0300

    new student app

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :students
+  root "home#index"
+  get "about", to: "home#about"
+  get "contact", to: "home#contact"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
